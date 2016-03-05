afd3a278de2f0eb201bc0b4e87ee58faad2d9412
tsubaki <tsubaki@moon>
2016-03-05 10:52:32 +0900

    Add Socket

---
+++ src/viola/net/Socket.cpp
@@ -0,0 +1,18 @@
+/*
+ * Socket.cpp
+ *
+ *  Created on: 2016/03/05
+ *      Author: tsubaki
+ */
+
+#include <net/Socket.h>
+
+Socket::Socket() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Socket::~Socket() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/viola/net/Socket.h
@@ -0,0 +1,15 @@
+/*
+ * Socket.h
+ *
+ *  Created on: 2016/03/05
+ *      Author: tsubaki
+ */
+
+#pragma once
+#include "Object.h"
+
+class Socket: public Object {
+public:
+	Socket();
+	virtual ~Socket();
+};
